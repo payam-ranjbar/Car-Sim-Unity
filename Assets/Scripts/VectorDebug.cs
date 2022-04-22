1e7246a85fe3682131ec7abcdb680c4ed29ea4b7
PM <payam.ranjbar@live.com>
2022-04-22 19:00:05 +0430

    An Arena for vector math visual experiments

--- Assets/Scripts/VectorDebug.cs
+++ Assets/Scripts/VectorDebug.cs
@@ -19,6 +19,12 @@ public class VectorDebug : MonoBehaviour
     [SerializeField] private float velocity = 40f;
     private void OnDrawGizmos()
     {
+        // AngleAndDotProduct();
+        ProjectionTest();
+    }
+
+    private void AngleAndDotProduct()
+    {
         var carPos = car.position;
         var nodePos = node.position;
         var nextNodePos = nextNode.position;
@@ -30,15 +36,15 @@ public class VectorDebug : MonoBehaviour
 
         Gizmos.color = Color.gray;
         Gizmos.DrawLine(carPos, nodePos);
-        
+
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(nodePos,  nodePos + (nodePos - carPos) );
+        Gizmos.DrawLine(nodePos, nodePos + (nodePos - carPos));
 
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(nodePos, nextNodePos);
-        
-        
-        var lookVector =nodePos - carPos;
+
+
+        var lookVector = nodePos - carPos;
 
         var nodeVector = nextNodePos - nodePos;
 
@@ -50,7 +56,36 @@ public class VectorDebug : MonoBehaviour
         style.fontSize = 20;
         Handles.Label(carPos + Vector3.right, $" angle : {angle}", style);
         Handles.Label(carPos + Vector3.left, $" dot : {dot}", style);
-
     }
-    
+
+    private void ProjectionTest()
+    {
+        var origin = car.position;
+        var dest = node.position;
+        var secondaryPoint = nextNode.position;
+
+        var vector1 = dest - origin;
+        var vector2 = secondaryPoint - origin;
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(origin, vector1);
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawRay(origin, vector2);
+
+        var project = Vector3.Project(vector1, vector2.normalized);
+
+        Gizmos.color = Color.yellow;
+        // Gizmos.DrawRay(origin, project);
+
+        var style = new GUIStyle();
+        style.fontSize = 5;
+        Handles.Label(origin + Vector3.left * 2, $" Point : {dest}", style);
+        // x - origin = projected
+        Handles.Label(origin + Vector3.right * 2, $" projected Point : {project - origin}", style);
+
+        var d = Vector3.RotateTowards(vector1, vector2.normalized, 2*Mathf.PI, vector1.magnitude);
+        
+        Gizmos.DrawRay(origin, d);
+    }
 }
